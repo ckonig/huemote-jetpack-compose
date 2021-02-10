242aed100f6f5cc517b5a59255151df60926d64a
ckonig <itckoenig@gmail.com>
2021-02-10 17:52:19 +0100

    basic tabnav with composable fragment child

---
+++ app/src/main/java/com/example/botnavc/ui/home/CustomComponent.kt
@@ -0,0 +1,9 @@
+package com.example.botnavc.ui.home
+
+import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomComponent() {
+return Text("hello world this is a custom Component");
+}
